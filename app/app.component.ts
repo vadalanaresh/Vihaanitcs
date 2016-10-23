4476ad8e801794b225ae27900b3be68746ae93bb
vadalanaresh <vadalanaresh@gmail.com>
2016-10-23 21:14:55 +0530

    Nav bar changes

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './nav/nav.component';
+@Component({
+    selector: 'my-app',
+    template: `<div>
+    sdkfhjkshdufhiasehfiywIUAHFKJHUIHIK
+</div>`
+})
+export class AppComponent {
+
+}
